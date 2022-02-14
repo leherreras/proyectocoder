1086b735fedb38d1ff47d1ffd2edbb4a1ccf160d
Luis Herrera <leherreras@gmail.com>
2022-02-14 09:58:18 -0500

    Create model to manage avatar in users

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Curso(models.Model):
 
     nombre = models.CharField(max_length=40, null=False)
-    camada = models.IntegerField(unique=True, null=False)
+    camada = models.IntegerField()
 
     def __str__(self):
         return f'El nombre del curso es: {self.nombre} con numero de camada: {self.camada}'
@@ -30,3 +32,9 @@ class Entregable(models.Model):
     nombre = models.CharField(max_length=40)
     fechaDeEntrega = models.DateField()
     entregado = models.BooleanField()
+
+
+# Create your models here.
+class Avatar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank=True)
